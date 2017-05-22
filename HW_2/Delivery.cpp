8f911b81d855f78d0a385fcf52534f22f314ae2d
armadolph <yalits4@gmail.com>
2017-05-22 19:17:15 +0300

    initialize the project

---
+++ HW_2/Delivery.cpp
@@ -0,0 +1,18 @@
+/*
+ * Delivery.cpp
+ *
+ *  Created on: May 22, 2017
+ *      Author: yali
+ */
+
+#include "Delivery.h"
+
+Delivery::Delivery() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Delivery::~Delivery() {
+	// TODO Auto-generated destructor stub
+}
+

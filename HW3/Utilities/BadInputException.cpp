976088bfa2a672a3d3c6be89285f6310c45ac007
armadolph <yalits4@gmail.com>
2017-07-02 21:28:59 +0300

    added an exception. more comfurable
    i think all the interface with the user through command line as been
    taken care of. a bit more work needed on controller in order to finish
    it.

---
+++ HW3/Utilities/BadInputException.cpp
@@ -0,0 +1,13 @@
+/*
+ * BadInputException.cpp
+ *
+ *  Created on: Jul 2, 2017
+ *      Author: yali
+ */
+
+#include "BadInputException.h"
+
+BadInputException::BadInputException(const char* whatArg):runtime_error(whatArg) {}
+
+BadInputException::~BadInputException() {}
+
--- HW3/Utilities/CommandInfo.cpp
+++ HW3/Utilities/CommandInfo.cpp
@@ -6,13 +6,15 @@
  */
 
 #include "CommandInfo.h"
+#include "BadInputException.h"
 #include <vector>
 #include <algorithm>
 #include <sstream>
+#include <array>
 
 using namespace std;
 
-bool CommandInfo::breakCommand(string line){
+void CommandInfo::breakCommand(string line){
 	vector<string> mvCMD = {"status","go","create","default","size","zoom","pan","show","quit"};
 	stringstream ss(line);
 	string buffer;
@@ -20,7 +22,7 @@ bool CommandInfo::breakCommand(string line){
 	char garbage;
 
 
-	ss >> buffer;
+	getline(ss,buffer,' ');
 	auto pos = find(mvCMD.begin(),mvCMD.end(),buffer) - mvCMD.begin();
 	switch (pos) {
 	case 0:
@@ -29,69 +31,60 @@ bool CommandInfo::breakCommand(string line){
 		// check validity of status command
 		ss >> garbage;
 		if(!ss.fail())
-			return false;
-		return true;
+			throw BadInputException("extra characters on status command");
+		return;
 	case 1:
 		cmdSec = CommandSection::MODEL;
 		cmd = Commands::GO;
 		// check validity of go command
 		ss >> garbage;
 		if(!ss.fail())
-			return false;
-		return true;
+			throw BadInputException("extra characters on go command");
+		return;
 	case 2:
 		cmdSec = CommandSection::MODEL;
 		cmd = Commands::CREATE;
 		// check validity of create command
 		getline(ss,buffer);
-		return createConditions(buffer);
+		createConditions(buffer);
+		return;
 	case 3:
 		cmdSec = CommandSection::VIEW;
 		cmd = Commands::DEFAULT;
 		// check validity of default command
 		ss >> garbage;
 		if(!ss.fail())
-			return false;
-		return true;
+			throw BadInputException("extra characters on default command");
+		return;
 	case 4:
 		cmdSec = CommandSection::VIEW;
 		cmd = Commands::SIZE;
 		// check validity of size command
-		ss >> tempArg;
-		if(ss.fail())
-			return false;
-		ss >> garbage;
-		if(!ss.fail())
-			return false;
+		getline(ss,buffer,' ');
+		getInt(buffer,tempArg);
 		arg1 = tempArg;
-		return true;
+		return;
 
 	case 5:
 		cmdSec = CommandSection::VIEW;
 		cmd = Commands::ZOOM;
 		// check validity of zoom command
-		ss >> arg1;
-		if(ss.fail())
-			return false;
-		ss >> garbage;
-		if(!ss.fail())
-			return false;
-		return true;
+		getline(ss,buffer,' ');
+		getInt(buffer,tempArg);
+		arg1 = tempArg;
+		return;
 
 	case 6:
 		cmdSec = CommandSection::VIEW;
 		cmd = Commands::PAN;
 		// check validity of pan command
-		ss >> arg1;
-		if(ss.fail())
-			return false;
-		ss >> arg2;
-		if(ss.fail())
-			return false;
-		ss >> garbage;
-		if(!ss.fail())
-			return false;
-		return true;
+		getline(ss,buffer,' ');
+		getInt(buffer,tempArg);
+		arg1 = tempArg;
+		getline(ss,buffer,' ');
+		getInt(buffer,tempArg);
+		arg2 = tempArg;
+		return;
 
 	case 7:
 		cmdSec = CommandSection::VIEW;
@@ -99,15 +92,15 @@ bool CommandInfo::breakCommand(string line){
 		// check validity of show command
 		ss >> garbage;
 		if(!ss.fail())
-			return false;
-		return true;
+			throw BadInputException("extra characters on show command");;
+		return ;
 	case 8:
 		cmdSec = CommandSection::QUIT;
 		// check validity of quit command
 		ss >> garbage;
 		if(!ss.fail())
-			return false;
-		return true;
+			throw BadInputException("extra characters on quit command");
+		return;
 	default:
 		cmdSec = CommandSection::SHIP;
 		break;
@@ -115,168 +108,105 @@ bool CommandInfo::breakCommand(string line){
 	shipName = buffer;
 	// get the rest of the text
 	getline(ss,buffer);
-	return shipCommand(buffer);
+	return shipCommandBreak(buffer);
 }
 
 
-bool CommandInfo::shipCommand(string line){
-	vector<string> shipCMD = {"course","position","destination","load_at","unload_at","dock_at","attack","refuel","stop"};
+void CommandInfo::shipCommandBreak(string line){
+	vector<string> shipCMDs = {"course","position","destination","load_at","unload_at","dock_at","attack","refuel","stop"};
 	stringstream ss(line);
 	string buffer;
 	int tempArg;
 	char garbage;
 
-	ss >> buffer;
-	auto pos = find(shipCMD.begin(),shipCMD.end(),buffer) - shipCMD.begin();
+	getline(ss,buffer,' ');		// get the command
+	auto pos = find(shipCMDs.begin(),shipCMDs.end(),buffer) - shipCMDs.begin();	// calculate which command
 	switch (pos) {
 		case 0:
 			shipCMD = COURSE;
-			ss >> arg1;
-			if(ss.fail())
-				return false;
-			ss >> arg2;
-			if(ss.fail())
-				return false;
-			ss >> garbage;
-			if(!ss.fail())
-				return false;
-			break;
+			getline(ss,buffer,' ');
+			getInt(buffer,tempArg);
+			arg1 = tempArg;
+			getline(ss,buffer,' ');
+			getInt(buffer,tempArg);
+			arg2 = tempArg;
+			return;
 
 		case 1:
 			shipCMD = POSITION;
-
-			ss >> buffer;
-			if(ss.fail())
-				return false;
-			if(!getCoordinate(buffer,tempArg,'('))
-				return false;
-			arg1 = tempArg;
-			ss >> buffer;
-			if(!getCoordinate(buffer,tempArg,')'))
-				return false;
-			arg2 = tempArg;
-			ss >> arg3;
-			if(ss.fail())
-				return false;
-			ss >> garbage;
-			if(!ss.fail())
-				return false;
-			break;
+			getline(ss,buffer,' ');
+			getXCoordinate(buffer,arg1);		// get x
+			getline(ss,buffer,' ');
+			getYCoordinate(buffer,arg2);		// get y
+			getline(ss,buffer,' ');
+			getDouble(buffer,arg3);				// get velocity
+			return;
 
 		case 2:
 			shipCMD = DESTINATION;
-			ss >> destination;
-			if(ss.fail())
-				return false;
-			ss >> arg1;
-			if(ss.fail())
-				return false;
-			ss >> garbage;
-			if(!ss.fail())
-				return false;
+			if(!getline(ss,destination,' '))
+				throw BadInputException("no destination has been entered");
+			getline(ss,buffer,' ');
+			getInt(buffer,tempArg);
+			arg1 = tempArg;
 			break;
 
 		case 3:
 			shipCMD = LOAD_AT;
-			ss >> destination;
-			if(ss.fail())
-				return false;
+			if(!getline(ss,destination,' '))
+				throw BadInputException("no destination has been entered");
 			ss >> garbage;
 			if(!ss.fail())
-				return false;
+				throw BadInputException("extra characters in LOAD_AT");
 			break;
 
 		case 4:
 			shipCMD = UNLOAD_AT;
-			ss >> destination;
-			if(ss.fail())
-				return false;
-			ss >> arg1;
-			if(ss.fail())
-				return false;
-			ss >> garbage;
-			if(!ss.fail())
-				return false;
+			if(!getline(ss,destination,' '))
+				throw BadInputException("no destination has been entered");
+			getline(ss,buffer,' ');
+			getInt(buffer,tempArg);
+			arg1 = tempArg;
 			break;
 
 		case 5:
 			shipCMD = DOCK_AT;
-			ss >> destination;
-			if(ss.fail())
-				return false;
+			if(!getline(ss,destination,' '))
+				throw BadInputException("no destination has been entered");
 			ss >> garbage;
 			if(!ss.fail())
-				return false;
+				throw BadInputException("extra characters in DOCK_AT");
 			break;
 		case 6:
 			shipCMD = ATTACK;
-			ss >> destination;
-			if(ss.fail())
-				return false;
+			if(!getline(ss,destination,' '))
+				throw BadInputException("no destination has been entered");
 			ss >> garbage;
 			if(!ss.fail())
-				return false;
+				throw BadInputException("extra characters in ATTACK");
 			break;
 		case 7:
 			shipCMD = REFUEL;
 			ss >> garbage;
 			if(!ss.fail())
-				return false;
+				throw BadInputException("extra characters in command refuel");
 			break;
 		case 8:
 			shipCMD = STOP;
 			ss >> garbage;
 			if(!ss.fail())
-				return false;
+				throw BadInputException("extra characters in command STOP");
 			break;
 		default:
-			break;
+			throw BadInputException("command does not exist");
 	}
-	return true;
-}
-
-// return true if succeed
-bool CommandInfo::getCoordinate(string buffer,int& arg,char c){
-	stringstream ss(buffer);
-	string tempBuff;
-	int pos;
-	char garbage;
-
-	ss >> garbage;
-	if(garbage != c)
-		return false;
-
-	getline(ss,tempBuff);
-	return getInt(tempBuff,arg);
-}
-
-bool getInt(string buffer,int& arg){
-	stringstream ss(buffer);
-		int pos;
-		char garbage;
-
-		ss >> pos;
-		if(ss.fail())
-			return false;
-		ss >> garbage;
-		if(!ss.fail())
-			return false;
-		arg = pos;
-		return true;
-}
-
-int getShipType(string type){
-	int numberOfShipTypes = 3,notExist=-1;
-	array<string,numberOfShipTypes> shipTypes = {"Freighter","Cruise_ship","Cruiser"};
-	for (int i = 0; i < numberOfShipTypes; ++i) {
-		if(type == shipTypes[i])
-			return i;
-	}
-	return notExist;
 }
 
 
-bool CommandInfo::createConditions(string line){
+
+
+
+void CommandInfo::createConditions(string line){
 
 	stringstream ss(line);
 	string buffer;
@@ -289,51 +219,124 @@ bool CommandInfo::createConditions(string line){
 				break;
 
 			case 2:
-				switch (getShipType(buffer)) {
-					case 0:
-						type = Ship::Type::FREIGHTER;
-						break;
-					case 1:
-						type = Ship::Type::CRUISE_SHIP;
-						break;
-					case 2:
-						type = Ship::Type::CRUISER;
-						break;
-					default:
-						return false;
-				}
+				setShipType(buffer);
 				break;
 
 			case 3:
-				if(!getCoordinate(buffer,tempArg,'('))
-					return false;
-				arg1 = tempArg;
+				getXCoordinate(buffer,arg1);	// get the x coordiante
 				break;
 			case 4:
-				if(!getCoordinate(buffer,tempArg,')'))
-					return false;
-				arg2 = tempArg;
+				getYCoordinate(buffer,arg2);// get the y coordiante
 				break;
 			case 5:
 				if(type == Ship::Type::CRUISE_SHIP)
-					return false;
-				if(!getInt(buffer,tempArg))
-					return false;
+					throw BadInputException("cruise ship needs only 4 arguments");
+				getInt(buffer,tempArg); // get resistance
 				arg3 = tempArg;
 				break;
 			case 6:
 				if(type == Ship::Type::CRUISE_SHIP)
-					return false;
-				if(!getInt(buffer,tempArg))
-					return false;
-				arg3 = tempArg;
+					throw BadInputException("cruise ship needs only 4 arguments");
+				getInt(buffer,tempArg);	// get
+				arg4 = tempArg;
 				break;
 			default:
-				return false;
+				throw BadInputException("too much arguments have been entered for create command");
 		}
 	}
+}
 
 
 
-	return true;
+void CommandInfo::setShipType(string buffer){
+	array<string,3> shipTypes = {"Freighter","Cruise_ship","Cruiser"};
+
+	if(buffer == shipTypes[0])
+		type = Ship::Type::FREIGHTER;
+	else if(buffer == shipTypes[1])
+		type = Ship::Type::CRUISE_SHIP;
+	else if(buffer == shipTypes[2])
+		type = Ship::Type::CRUISER;
+	else{
+		throw BadInputException((buffer + " is an illegal ship type on create").c_str());
+	}
+
+}
+
+
+
+
+// return true if succeed
+void CommandInfo::getXCoordinate(string buffer,double& arg){
+	stringstream ss(buffer);
+	string tempBuff;
+	char garbage;
+
+	ss >> garbage;
+	if(garbage != '('){//create Ajax Cruiser (15.00 15.00) 6 1{
+		throw BadInputException("the x coordinate is bad");
+	}
+
+	ss >> arg;
+	if(ss.fail())
+		throw BadInputException("double as not been entered properly");
+	ss >> garbage;
+	if(!ss.fail())
+		if(garbage != ',')
+			throw BadInputException("no , between coordinates");
+	ss >> garbage;
+	if(!ss.fail())
+		throw BadInputException("extra characters in x coordinate");
+
+
+}
+
+
+void CommandInfo::getYCoordinate(string buffer,double& arg){
+	stringstream ss(buffer);
+	string tempBuff;
+	char garbage;
+
+	ss >> arg;
+	if(ss.fail())
+		throw BadInputException("double as not been entered properly");
+	ss >> garbage;
+	if(garbage != ')')
+		throw BadInputException("coordinates does not end with )");
+	ss >> garbage;
+	if(!ss.fail())
+		throw BadInputException("extra characters in x coordinate");
+
+
+}
+
+
+
+void CommandInfo::getInt(string buffer,int& arg){
+	stringstream ss(buffer);
+	int temp;
+	char garbage;
+
+	ss >> temp;
+	if(ss.fail())
+		throw BadInputException("int as not been entered properly");
+	ss >> garbage;
+	if(!ss.fail())
+		throw BadInputException("extra chars in input");
+	arg = temp;
+}
+
+
+void CommandInfo::getDouble(string buffer,double& arg){
+	stringstream ss(buffer);
+	double temp;
+	char garbage;
+
+	ss >> temp;
+	if(ss.fail())
+		throw BadInputException("double as not been entered properly");
+	ss >> garbage;
+	if(!ss.fail())
+		throw BadInputException("extra chars in input");
+	arg = temp;
 }

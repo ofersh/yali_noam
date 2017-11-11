def2a547ef7843246e2c0d73eb961cd1af93e5cb
Noam Stolero <stellern@gmail.com>
2017-11-11 14:13:26 +0200

    fast networks hw1 started

--- fast_networks/hw1/hw1/dn_protocol.cpp
+++ fast_networks/hw1/hw1/dn_protocol.cpp
@@ -1,24 +1,67 @@
-//
-//  dn_protocol.cpp
-//  hw1
-//
-//  Created by Noam Stolero on 10/11/2017.
-//  Copyright © 2017 Noam Stolero. All rights reserved.
-//
 
 #include "dn_protocol.h"
 #include <fstream>
+#include <iostream>
+
 using namespace std;
 
-const int PACKET_LENGTH = 256;
 
-void Dn_protocol::prepare_file(string filename){
-    fstream fin{filename, ios_base::in | ios_base::binary};
-    char packet[PACKET_LENGTH];
-    
-    while (int true_length = fin.read(packet, PACKET_LENGTH)) {
-        <#statements#>
+bool Dn_protocol::file_to_ip_packets(string input_file_name, string output_file_name, unsigned int source_ip, unsigned int destination_ip){
+    fstream fin{ input_file_name, ios_base::in | ios_base::binary};
+	fstream fout{ output_file_name, ios_base::out | ios_base::binary };
+
+	char packet[PACKET_LENGTH] = {0};
+	char packet_with_iph[PACKET_LENGTH + HEADER_LENGTH];
+	long read_size = 0;
+
+	int read_bytes = 0;
+
+	IP_HEADER iph;
+	iph.source = source_ip;
+	iph.destination = destination_ip;
+
+
+	while (fin.read(packet, PACKET_LENGTH)) 
+    {
+		read_size = fin.tellg() % PACKET_LENGTH;
+		if (read_size == 0)
+			 read_size = PACKET_LENGTH;
+
+		read_bytes += read_size;
+		iph.total_length = (unsigned short) read_size + HEADER_LENGTH;
+		calculate_checksum(iph);
+		fout.write(reinterpret_cast<char*>(&iph), HEADER_LENGTH);
+		fout.write(packet, read_size);
     }
-    
-    
+
+	cout << "the program read " << read_bytes << " bytes" << endl;
+	return true;
+}
+
+bool Dn_protocol::ip_packets_to_file(string input_file_name, string output_file_name, unsigned int source_ip, unsigned int destination_ip)
+{
+	return false;
+}
+
+
+const void Dn_protocol::calculate_checksum(IP_HEADER &iph)
+{
+	// Calculate ip header checksum by Words
+
+	int checksum = 0;
+	short *words_p;
+	words_p = reinterpret_cast<short*>(&iph);
+
+	iph.header_checksum = 0;
+
+	for (int i = 0; i < HEADER_LENGTH / 2; i++, words_p++)
+	{
+		checksum += *words_p;
+	}
+	iph.header_checksum = ~checksum;
+}
+
+const void Dn_protocol::write_packet(IP_HEADER, char packet[PACKET_LENGTH], char * target)
+{
+	
 }

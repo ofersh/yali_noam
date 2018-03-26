f46c9096dd2ccd77add5596e65071098e234fdf9
Noam Stolero <stellern@gmail.com>
2018-03-26 23:00:05 +0300

    finished 3 computer vision

--- y3/sem_b/computer vision/hw1/global_boundary.m
+++ y3/sem_b/computer vision/hw1/global_boundary.m
@@ -1,20 +1,21 @@
 load('sq.mat');
-sq = uint8(sq);
+load('sq1.mat');
 
-figure(1), subplot(2,2,1), histogram(sq), title('Original histogram');
+sq_sigma = get_maximal_sigma(sq, 'SQ');
+sq1_sigma = get_maximal_sigma(sq1, 'SQ1');
 
-sigma = 10/255;
-gaus_10 = imnoise(sq, 'gaussian', 0, sigma^2);
-figure(1), subplot(2,2,2), histogram(gaus_10), title('Gaussian sigma=10 histogram');
-fprintf('a. The global boundry can be found without error\n')
+fprintf('\n')
+fprintf('\n')
+fprintf('') 
 
-sigma = 15/255;
-gaus_15 = imnoise(sq, 'gaussian', 0, sigma^2);
-figure(1), subplot(2,2,3), histogram(gaus_10), title('max Gaussian noise');
-fprintf('b. The global error free boundry can be found with max sigma 15 gaussian noise\n')
+%{
+d. the maximum sigma can be calculated with the following formula:
 
-sigma = 50/255;
-gaus_50 = imnoise(sq, 'gaussian', 0, sigma^2);
-figure(1), subplot(2,2,4), histogram(gaus_50), title('Gaussian sigma=50 histogram');
-fprintf('c. The global boundry can not be found\n')
+    max_sigma = (mean2(sq) - mean(sq(sq<mean2(sq)))) / 3 - 1
+
+    we know that beyond 3 sigma border, lies 0.1% of the pixels
+    threfore we calculate the distance between lower "modle" and the mean divide by 3
+%}
+
+
 

09f2ffdc0ff7fb283bf7aff4a7c88fb2ce2e5d9e
armadolph <yalits4@gmail.com>
2018-05-04 14:05:06 +0300

    Added question 8. prepared question 3 and 5 for submition. Need to review question 8.

--- y3/sem_b/computer_vision/hw2/q3/question3.m
+++ y3/sem_b/computer_vision/hw2/q3/question3.m
@@ -1,17 +1,17 @@
+%% Question 3
+% Calculating the circularity for each object in the images:
+%  cell1.bmp, cell2.bmp
 clc, clear all;
 
+%% cell1.bmp
 image = imread('cell1.bmp');
-
 [seg_im, vals] = segmentation(image);
-
-fprintf('q3\n')
-fprintf('For cell1.bmp:\n')
 for val=vals(1 : end-1)
     c = circularity(seg_im, val);
     fprintf('For object of value %3g the circularity is %.3g\n', val, c)
 end
 
-fprintf('\nFor cell2.bmp:\n')
+%% cell2.bmp
 image = imread('cell2.bmp');
 [seg_im, vals] = segmentation(image);
 for val=vals(1 : end-1)
@@ -19,4 +19,48 @@ for val=vals(1 : end-1)
     fprintf('For object of value %3g the circularity is %.3g\n', val, c)
 end
 
+
+%% Calculating Circularity Function:
+%   function [ c ] = circularity( image, val )
+% % circularity Calculates circularity for segmented shape.
+% % c = circularity calculation.
+% % image = segmented image.
+% % val = segmented shape value.
+% 
+%   area = length(find(image == val));
+%   perim = myPerim(image, val);
+%   c = (4*pi*area)/perim^2;
+%   end
+
+%% Laplace perimeter Calculation Function
+%   function [ perim ] = myPerim( image, val )
+%      perim = 0;
+%      [nrows, ncols] = size(image);
+%      for x = 1:ncols
+%          for y = 1:nrows
+%              if image(y, x) == val
+%                  k = 0;
+%                  for i = -1:1
+%                      for j = -1:1
+%                          if x+i < 1 || y+j < 1 
+%                              continue
+%                          end
+%                          if image(y+j, x+i) == 255
+%                              k = k + 1;
+%                              break;
+%                          end
+%                      end
+%                      if k ~= 0
+%                          break
+%                      end
+%                  end
+%                  if k ~= 0
+%                      perim = perim + 1;
+%                      k = 0;
+%                  end
+%              end
+%          end
+%      end
+% 
+%   end
 
--- y3/sem_b/computer_vision/hw2/q5/q5.m
+++ y3/sem_b/computer_vision/hw2/q5/q5.m
@@ -1,23 +1,27 @@
-clc; clear all;
+%% Question 5
+% Hough Transformation
+
+clc; clear all, close all;
 
 nPoints = 1:3;
-% a)
+%% A:
 % Draw one point transformation
 x = 20; y = 10;
 figure('Name','One point');
 houghTrans(x, y);
+title('One point')
 
 
-% b)
+%% B:
 % Draw transformation of points in thbe same line
 figure('Name','Same line');
 y = @(x) 2*x;
 for point = nPoints
    houghTrans(point, y(point)); hold on 
 end
+title('Same line')
 
-% c)
-
+%% C:
 % Draw parallel lines trasformation
 figure('Name','Parallel lines');
 y1 = @(x) x;
@@ -28,6 +32,7 @@ end
 for point = nPoints
    houghTrans(point, y2(point)); hold on 
 end
+title('Parallel lines')
 
 % Draw vertical lines trasformation
 figure('Name','Perpendicular Lines');
@@ -37,6 +42,18 @@ end
 for point = nPoints
    houghTrans(0, point); hold on 
 end
+title('Perpendicular lines')
+
+
+%% Hough Tranformation Function:
+%   function  houghTrans(x, y )
+%      theta = 0:180;
+%      r =  x * cos(theta) + y * sin(theta);
+%      plot(theta, r);
+% 
+%   end
+
+
 
 
 

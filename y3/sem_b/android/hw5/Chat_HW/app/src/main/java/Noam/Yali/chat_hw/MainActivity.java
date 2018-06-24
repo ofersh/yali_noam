19486df44bc13bc863408a385c5871cf90d6002c
armadolph <yalits4@gmail.com>
2018-06-24 15:44:12 +0300

    [Android - HW5] Finished all the functionalities. Need to work on the appearence

--- y3/sem_b/android/hw5/Chat_HW/app/src/main/java/Noam/Yali/chat_hw/MainActivity.java
+++ y3/sem_b/android/hw5/Chat_HW/app/src/main/java/Noam/Yali/chat_hw/MainActivity.java
@@ -2,9 +2,16 @@ package Noam.Yali.chat_hw;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +30,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,10 +41,20 @@ public class MainActivity extends AppCompatActivity {
     EditText messageBox;
     MessagesListAdapter adapter;
 
+    ArrayList<String> curses;
+    ArrayList<String> compliments;
+    ArrayList<String> markWords;
+    ArrayList<String> filterWords;
+    Boolean markCompliments = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        curses = allCurses();
+        compliments = allCompliments();
+        boldComplimentsfilterCurses();
+
 
         if(FirebaseAuth.getInstance().getCurrentUser() == null){
             startActivity(new Intent(this, LoginActivity.class));
@@ -48,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
         messages = new ArrayList<>();
         listView = findViewById(R.id.listView);
-        adapter = new MessagesListAdapter(this, messages);
+        adapter = new MessagesListAdapter(this, messages, this);
         listView.setAdapter(adapter);
 
         messageBox = findViewById(R.id.messageBox);
@@ -82,6 +100,14 @@ public class MainActivity extends AppCompatActivity {
         closeKeyboard(view);
     }
 
+    public void switchEditor(View view) {
+        if(markCompliments)
+            boldCursesfilterCompliments();
+        else
+            boldComplimentsfilterCurses();
+        adapter.notifyDataSetChanged();
+    }
+
     private void closeKeyboard(View view){
         view.clearFocus();
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -97,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
     public static class MessagesListAdapter extends ArrayAdapter<Message> {
 
         private ArrayList<Message> messages;
+        private MainActivity messagesEditor;
 
         private static class ViewHolder {
             TextView messageTV;
@@ -104,9 +131,10 @@ public class MainActivity extends AppCompatActivity {
             TextView senderTV;
         }
 
-        public MessagesListAdapter(Context context, ArrayList<Message> messages) {
+        public MessagesListAdapter(Context context, ArrayList<Message> messages, MainActivity messagesEditor) {
             super(context, R.layout.messages_list_item, messages);
             this.messages = messages;
+            this.messagesEditor = messagesEditor;
         }
 
         SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
@@ -127,11 +155,86 @@ public class MainActivity extends AppCompatActivity {
                 viewHolder = (ViewHolder) convertView.getTag();
             }
 
-            viewHolder.messageTV.setText(message.message);
+            SpannableString editedMessage = messagesEditor.editMessage(message.message);
+            viewHolder.messageTV.setText(editedMessage, TextView.BufferType.SPANNABLE);
             viewHolder.dateTV.setText(dateFormatter.format(message.date));
             viewHolder.senderTV.setText(message.sender + " says:");
 
             return convertView;
         }
     }
+
+    private ArrayList<String> allCompliments() {
+        ArrayList<String> compliments = new ArrayList<>();
+        compliments.add("beautiful");
+        compliments.add("strong");
+        compliments.add("smart");
+        return compliments;
+    }
+
+    private ArrayList<String> allCurses() {
+        ArrayList<String> curses = new ArrayList<>();
+        curses.add("ugly");
+        curses.add("weak");
+        curses.add("stupid");
+        return curses;
+    }
+
+    public SpannableString editMessage(String message){
+
+        // Initialize variables that will be used to edit the text
+        SpannableString newMessage = new SpannableString(message);
+
+        // Mark the words according to the configuration
+        for(String word: markWords){
+            StyleSpan boldStyle = new StyleSpan(Typeface.BOLD);
+            int wordStartIndex = message.indexOf(word);
+            int wordEndIndex = wordStartIndex + word.length();
+            if(wordStartIndex >= 0){
+                newMessage.setSpan(boldStyle, wordStartIndex, wordEndIndex, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            }
+        }
+
+        // Filter the words according to the configuration
+        for(String word: filterWords){
+            StrikethroughSpan strikethroughSpan = new StrikethroughSpan();
+            int wordStartIndex = message.indexOf(word);
+            int wordEndIndex = wordStartIndex + word.length();
+            if(wordStartIndex >= 0){
+                newMessage.setSpan(strikethroughSpan, wordStartIndex, wordEndIndex, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            }
+        }
+        return newMessage;
+    }
+
+//    private SpannableString markWords(SpannableString message){
+//        for(String word: markWords){
+//            int wordStartIndex = message.
+//        }
+//
+//        return newMessage;
+//    }
+//
+//    private SpannableString filterWords(SpannableString message){
+//
+//        for(String word: filterWords){
+//            String filtered = new String(new char[word.length()]).replace("\0", "*");
+//            newMessage = newMessage.replace(word, filtered);
+//        }
+//
+//        return newMessage;
+//    }
+
+    private void boldComplimentsfilterCurses(){
+        markWords = compliments;
+        filterWords = curses;
+        markCompliments = true;
+    }
+
+    private void boldCursesfilterCompliments(){
+        markWords = curses;
+        filterWords = compliments;
+        markCompliments = false;
+    }
+
 }
